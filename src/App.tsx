e22d549db426745d5d8e1ed297b158e5da84162b
Tiziano Di Felice <tizianofelice@outlook.it>
2025-02-10 10:02:14 +0100

    sistemata questione dello scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,6 +7,17 @@ import Home from "@/pages/home";
 import Articles from "@/pages/articles";
 import Coaching from "@/pages/coaching";
 import Social from "@/pages/social";
+import React from "react";
+
+function ScrollToTop() {
+  const [location] = useLocation();
+  
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  
+  return null;
+}
 
 function Router() {
   return (
@@ -23,6 +34,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ScrollToTop />
       <Router />
       <Toaster />
     </QueryClientProvider>
